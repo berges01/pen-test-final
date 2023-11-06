463216834a567981fcfbeb107051ebcab3f3fb0e
Erik Berges <erik.t.berges@gmail.com>
2023-11-05 17:01:41 -0700

    updated generate_passwords and passwords

--- generate_passwords.py
+++ generate_passwords.py
@@ -2,7 +2,7 @@ def number_generator():
 
     file_path = 'passwords.txt'
     with open(file_path, 'a') as file:
-        for i in range(1, 999999):
+        for i in range(1, 1000000):
             formatted_number = f"{i:06d}\n"
             file.write(formatted_number)
 
